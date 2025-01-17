a1296f3decbfb1ac3f0f31b710851ea834f36af9
Giulia <giuliadevweb@users.noreply.github.com>
2025-01-17 11:49:46 +0100

    create migration and model for Job

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,36 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
+class Job extends Model {
 
-    public static function all (): array
-    {
-        return [
-            [  'id' => 1,
-                'title' => 'Directory',
-                'salary' => '$5000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$2500'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Ux Designer',
-                'salary' => '$1500'
-            ]
-        ];
-    }
+    //serve solo se il modello non lo chiamiamo come la migrazione, in uesto caso si doveva chiamare JobListing
+    protected $table = 'job_listings';
+    // Specifica i campi che possono essere assegnati in modo massivo
+    protected $fillable = ['title', 'salary'];
 
-    public static function find(int $id): array
-    {
-       $job = Arr::first(static::all(),fn($job) => $job['id'] == $id);
-       if(!$job){
-            abort(404);
-       }
-        return $job;
-    }
+    //METODO CHE PRENDE I DATI DIRETTAMENTE DA CODICE; FISSI (non serve mettere extends Model)
+        // public static function all (): array
+        // {
+        //     return [
+        //         [  'id' => 1,
+        //             'title' => 'Directory',
+        //             'salary' => '$5000'
+        //         ],
+        //         [
+        //             'id' => 2,
+        //             'title' => 'Programmer',
+        //             'salary' => '$2500'
+        //         ],
+        //         [
+        //             'id' => 3,
+        //             'title' => 'Ux Designer',
+        //             'salary' => '$1500'
+        //         ]
+        //     ];
+        // }
+
+        // public static function find(int $id): array
+        // {
+        //    $job = Arr::first(static::all(),fn($job) => $job['id'] == $id);
+        //    if(!$job){
+        //         abort(404);
+        //    }
+        //     return $job;
+        // }
 }
